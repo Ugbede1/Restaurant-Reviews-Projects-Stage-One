1a5327aae01c56563ede1658c13b67b32364be44
Ugbede Achile <achilelive@gmail.com>
2018-11-21 20:12:29 +0100

    Update service worker

--- sw.js
+++ sw.js
@@ -43,12 +43,15 @@ self.addEventListener('activate', function(event) {
 		})
 	);
 })
-
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
